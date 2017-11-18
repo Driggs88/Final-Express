22f2b3689f32d1976db6acd0b5dbeb5655ddb3f9
Driggs88 <riggs_david@hotmail.com>
2017-11-18 16:59:38 -0500

    Signup and Login routes added

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -7,6 +7,7 @@ const User = require('../models/user-model');
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => {
+                            // reference name from the form
   const username = req.body.username;
   const password = req.body.password;
 
@@ -27,13 +28,37 @@ router.post('/signup', (req, res, next) => {
     const theUser = new User({
       username: username,
       password: hashPass,
-    }),
-    theUser.save((err) => {
-      
-    });
+    })
+        theUser.save((err) => {
+            if (err) {
+              res.status(500).json({ message:  'Something went wrong' });
+              return;
+            }
+            req.login(theUser, (err) => {
+                res.status(200).json(theUser);
+        });
+     });
   });
 });
 
+router.post('/login', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ username: username}, (err, foundUser) =>{
+    if (foundUser === null) {
+        res.status(400).json({ message: 'Incorrect username'});
+        return;
+    }
+    if (!bcrypt.compareSync(password, foundUser.password)) {
+      res.status(400).json({ message: 'Incorrect password'});
+      return;
+    }
+    req.login(foundUser, (err) => {
+        res.status(200).json(foundUser);
+    });
+  })
+})
 
 
 
