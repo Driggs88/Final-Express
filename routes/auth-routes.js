fbc268f18d059f3a77aaea5193d9dd657a790a1c
Driggs88 <riggs_david@hotmail.com>
2017-12-25 08:55:16 -0500

    switch auth-routes to promise

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,101 +1,124 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const User = require('../models/user-model');
+const passport = require('passport');
 const multer    = require('multer');
 const upload = multer({ dest: 'public/uploads/' });
 
+
+const User = require('../models/user-model');
+
+
 const authRouter = express.Router();
 
-authRouter.post('/signup', (req, res, next) =>{
-  const profilePic = `/uploads/${req.file.filename}`;
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const username = req.body.username;
-  const email = req.body.email;
-  const password = req.body.password;
 
- if(!username || !password) {
-    res.status(400).json({message: 'Provide username and password'});
+authRouter.post('/api/signup', (req, res, next) => {
+  if (!req.body.signupEmail || !req.body.signupPassword) {
+    // 400 for client errors
+    res.status(400).json({ message: 'Need both email and password'});
     return;
   }
-    //see if the username is already taken (query the database)
-    User.findOne({ username: username }, '_id', (err, foundUser)=>{
-    if(foundUser) {
-      res.status(400).json({message: 'The username already exists!'});
-      return;
-    }
-    //save to the DB if we didn't find the user
-    const salt = bcrypt.genSaltSync(10);
-    const hashPass= bcrypt.hashSync(password, salt);
 
-    //create User
-    const theUser = new User({
-      profilePic: profilePic,
-      firstName: firstName,
-      lastName: lastName,
-      username: username,
-      email: email,
-      password: hashPass,
-    })
-
-    //save User
-    theUser.save((err) => {
-        if(err) {
-            res.stataus(500).json({message: 'Something went wrong'});
-        }
-        req.login(theUser, (err) => {
-          theUser.password = undefined;
-          res.status(200).json(theUser);
-        })//LOGIN
-    });//User.Save
-  });//User.findOne
-});//GET SIGNUP ROUTE
-
- authRouter.post('/login', (req, res, next) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-   //see if the username credential is valid
-    User.findOne({ username: username}, (err, foundUser) => {
-      //send an error if no user with that username
-      if(!foundUser) {
-          res.status(400).json({message: 'Incorrect Username'});
-          return;
-      }
-      if(!bcrypt.compareSync(password, foundUser.password)){
-        res.status(400).json({message: "Incorrect password!"});
+UserModel.findOne(
+    { email: req.body.signupEmail },
+    (err, userFromDb) => {
+      if (err) {
+        res.status(500).json({ message: 'Email check failed'});
         return;
       }
-      //if we get here we are GOOD!
-      //log the user in
-      req.login(foundUser, (err) => {
-        //hide the password from the frontend by setting it to undefined
-        foundUser.password = undefined;
-        res.status(200).json(foundUser);
+
+      if (userFromDb) {
+        // 400 for client errors
+        res.status(400).json({ message: 'Email already exists!' });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(10);
+      const scramblePassword = bcrypt.hashSync(req.body.signupPassword, salt);
+
+      const theUser = new UserModel ({
+        fullName: req.body.signupFullName,
+        email: req.body.signupEmail,
+        encryptedPassword: scramblePassword
+      }),
+
+      theUser.save((err) => {
+        if (err) {
+          res.status(500).json( { message: 'User cannot be saved'});
+          return;
+        }
+
+        // Automatically logs user in after the sign up
+        // (req.login() is defined by passport)
+        req.login(theUser, (err) => {
+          if (err) {
+            res.status(500).json({ message: 'Login failed'});
+            return;
+          }
+          // Clears encryptedPassword before sending from the object not database
+          theUser.encryptedPassword = undefined;
+
+          // Send the user's info to the frontend
+          res.status(200).json(theUser);
+        }); // close req.login()
+      }); //close theUser.save()
+    }
+  ); // close UserModel.findOne()
+}); // close authRouter.post('/signup')
+
+
+authRouter.post('/api/login', (req, res, next) => {
+    const authenticateFunction  =
+      passport.authenticate('local', (err, theUser, extraInfo) => {
+        // Errors prevented us from deciding if login was a success or failure
+        if (err) {
+          res.status(500).json({ message: 'Unknow login error' });
+          return;
+        }
+
+        // Login failed if theUser is empty
+        if (!theUser) {
+          // "extraInfo" contains feedback message from LocalStrategy
+          res.status(401).json(extraInfo);
+          return;
+        }
+
+        // Login successful, save them in the session
+        req.login(theUser, (err) => {
+          if (err) {
+            res..status(500).json({ message: 'Session failed to save'});
+            return;
+          }
+
+          // Clears encryptedPassword before sending from the object not database
+          theUser.encryptedPassword = undefined;
+
+          // Everything worked! Send the user's info to the client
+          res.status(200).json(theUser);
+        });
       });
-      //send an error if password
-    })//User.findOne()
-  }) //POST/login
 
-authRouter.post('/logout', (req, res, next) => {
+    authenticateFunction(req, res, next);
+});
+
+
+authRouter.post('/api/logout', (req, res, next) => {
+  // req.logout() is defined by passport
   req.logout();
-  res.status(200).json({message: 'Success'});
+  res.status(200).json({message: 'Log out success!'});
 });
 
-authRouter.get('/loggedin', (req, res, next) => {
-  if (req.isAuthenticated()) {
-    res.status(200).json(req.user);
-    return;
-  }
-  res.status(403).json({message: 'Unauthorized'})
-});
 
-authRouter.get('/private', (req, res, next) => {
-  if (req.isAuthenticated()) {
-    res.status(200).json({message: 'This is a private message'})
+authRouter.get('/api/checklogin', (req, res, next) => {
+  if (!req.user) {
+    res.status(401).json({ message: 'User is not logged in' });
     return;
   }
-  res.status(403).json({message: 'Unauthorized'})
+
+
+  // Clears encryptedPassword before sending from the object not database
+  theUser.encryptedPassword = undefined;
+
+  res.status(200).json(req.user);
 });
 
 
